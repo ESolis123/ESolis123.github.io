456482b30d92b020209b2d94df996b8a74da3b90
Enrique Solís <jose11@windowslive.com>
2025-11-17 20:40:57 -0400

    Botón para subir el scroll

--- js/loader.js
+++ js/loader.js
@@ -1,17 +1,31 @@
-document.addEventListener("DOMContentLoaded", ()=>{
-    agregarEventos();
-    cargarPagina("/contenido/main.html");
-
-})
-
-function agregarEventos(){
-    let dict = new Map();
+   let dict = new Map();
     dict.set("nav-inicio", "/contenido/main.html");
     dict.set("nav-paginas-web", "/contenido/paginas-web.html");
     dict.set("nav-videojuegos", "/contenido/videojuegos.html");
     dict.set("nav-programas-escritorio", "/contenido/programas.html");
     dict.set("nav-diseño-grafico", "/contenido/diseño-grafico.html");
     dict.set("nav-sobre-mi", "/contenido/about-me.html");
+
+document.addEventListener("DOMContentLoaded", ()=>{
+    agregarEventos();
+    cargarPagina("/contenido/main.html");
+    let botonSubirArriba = document.querySelector("#boton-ir-arriba");
+    botonSubirArriba.addEventListener("click", ()=>{
+        subirScroll();
+    })
+})
+
+window.addEventListener("scroll", ()=>{
+    let botonArriba = document.querySelector("#boton-ir-arriba");
+    if(scrollY === 0)
+        botonArriba.dataset.habilitado = "false";
+    else
+        botonArriba.dataset.habilitado = "true";
+
+
+})
+function agregarEventos(){
+
 
     let botones = [];
 
@@ -21,22 +35,39 @@ function agregarEventos(){
     }
 
     for(let [id, value] of dict.entries()){
-        let otrosBotones = botones.filter(boton => boton.id !== id);
         let button = document.querySelector("#" + id);
 
         button.addEventListener("click", ()=>{
             cargarPagina(value);
-            button.dataset.seleccionado = "true";
-
-            for(let boton of otrosBotones)
-                boton.dataset.seleccionado = "false";
         })
     }
 }
 
 async function cargarPagina(nombre){
     let contenido = document.getElementById("contenido");
+    let pagina;
+    let otrosBotones = [];
+    dict.forEach((value, key) => {
+        console.log(`Value: ${value} Key: ${key} Nombre: ${nombre} ${value===nombre}`)
+        if(value === nombre)
+            pagina = key
+
+        else
+            otrosBotones.push(document.querySelector("#"+key));
+    });
+
+    let navBoton = document.querySelector("#"+pagina);
+    navBoton.dataset.seleccionado = "true";
+
+    otrosBotones.forEach(boton => boton.dataset.seleccionado = "false");
+
     await fetch(`${nombre}`).then(data => data.text())
     .then(html => contenido.innerHTML = html);
     CambiarLabels();
+    subirScroll();
+
+}
+
+function subirScroll(){
+    window.scrollTo({ top: 0, behaviour: "smooth"})
 }
